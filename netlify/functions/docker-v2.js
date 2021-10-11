f41d773e7957d1bb47ab7bcbea4d6dfd6a6e06c4
Jens Langhammer <jens.langhammer@beryju.org>
2021-10-11 12:20:37 +0200

    more headers

--- netlify/functions/docker-v2.js
+++ netlify/functions/docker-v2.js
@@ -4,7 +4,9 @@ exports.handler = async function (event, context) {
     return {
         statusCode: 200,
         headers: {
-            "www-authenticate": `Bearer realm="https://${event.headers.host}/token",service="${event.headers.host}",scope="repository:beryju/acme-for-appliances:pull"`
+            "www-authenticate": `Bearer realm="https://${event.headers.host}/token",service="${event.headers.host}",scope="repository:beryju/acme-for-appliances:pull"`,
+            "Docker-Distribution-API-Version": "registry/2.0",
+            "content-type": "application/json",
         },
         body: JSON.stringify({})
     };
