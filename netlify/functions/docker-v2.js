2f703cdd786d67dbffdc0709f973a249465914a7
Jens Langhammer <jens.langhammer@beryju.org>
2021-10-12 09:33:44 +0200

    merge to single function

--- netlify/functions/docker-v2.js
+++ netlify/functions/docker-v2.js
@@ -1,10 +1,26 @@
+const config = {
+    githubUserOrg: "beryju",
+};
+
+async function getToken(event) {
+    const fetch = await import('node-fetch');
+    const upstreamScope = event.queryStringParameters["scope"];
+    const repo = upstreamScope.split(":");
+    const scope = `repository:${config.githubUserOrg}/${repo}:pull`;
+    const tokenRes = await fetch.default(`https://ghcr.io/token?service=ghcr.io&scope=${scope}`);
+    return {
+        statusCode: 200,
+        body: await tokenRes.text()
+    };
+}
+
 exports.handler = async function (event, context) {
     console.log(event);
     console.log(context);
     return {
-        statusCode: 200,
+        statusCode: 401,
         headers: {
-            "www-authenticate": `Bearer realm="https://${event.headers.host}/token",service="${event.headers.host}",scope="repository:beryju/acme-for-appliances:pull"`,
+            "www-authenticate": `Bearer realm="https://${event.headers.host}/token",service="${event.headers.host}",scope="repository:user/image:pull"`,
             "Docker-Distribution-API-Version": "registry/2.0",
             "content-type": "application/json",
         },
