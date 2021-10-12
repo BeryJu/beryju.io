914d4e121f246318c670f300ad5de616fd53b52d
Jens Langhammer <jens.langhammer@beryju.org>
2021-10-12 09:41:02 +0200

    add routing

--- netlify/functions/docker-v2.js
+++ netlify/functions/docker-v2.js
@@ -1,13 +1,15 @@
 const config = {
-    githubUserOrg: "beryju",
+    namespace: "beryju/",
+    registry: "ghcr.io",
+    registryTokenEndpoint: "ghcr.io/token"
 };
 
 async function getToken(event) {
     const fetch = await import('node-fetch');
     const upstreamScope = event.queryStringParameters["scope"];
     const repo = upstreamScope.split(":");
-    const scope = `repository:${config.githubUserOrg}/${repo}:pull`;
-    const tokenRes = await fetch.default(`https://ghcr.io/token?service=ghcr.io&scope=${scope}`);
+    const scope = `repository:${config.namespace}${repo}:pull`;
+    const tokenRes = await fetch.default(`https://${config.registryTokenEndpoint}?service=${config.registry}&scope=${scope}`);
     return {
         statusCode: 200,
         body: await tokenRes.text()
@@ -15,6 +17,9 @@ async function getToken(event) {
 }
 
 exports.handler = async function (event, context) {
+    if (event.queryStringParameters.hasOwnProperty("token")) {
+        return await getToken(event);
+    }
     console.log(event);
     console.log(context);
     return {
