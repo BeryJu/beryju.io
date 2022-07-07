55d2e2127ab82d210d8d673658107a38238a33a5
Jens Langhammer <jens.langhammer@beryju.org>
2022-07-07 14:15:04 +0000

    fix

--- netlify/functions/go-get.js
+++ netlify/functions/go-get.js
@@ -1,13 +1,12 @@
 const gitHubNamespace = "BeryJu";
 
 exports.handler = async function (event, context) {
-    console.log(event);
-    console.log(context);
+    const path = event.queryStringParameters.path;
     return {
         statusCode: 200,
         headers: {
             "content-type": "text/html",
         },
-        body: `<meta name="go-import" content="${event.headers.host}${event.path} git https://github.com/${gitHubNamespace}${event.path}">`
+        body: `<meta name="go-import" content="${event.headers.host}${path} git https://github.com/${gitHubNamespace}${path}">`
     };
 };
